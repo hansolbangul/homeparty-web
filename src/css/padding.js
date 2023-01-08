c8d67ff81670182c2ca1cf20fc94d12c924d154e
hansolbangul <ruaenddl981028@naver.com>
2023-01-09 00:37:44 +0900

    FEAT: padding css add

---
+++ src/css/padding.js
@@ -0,0 +1,19 @@
+export const margin_bottom_16 = {
+    marginBottom: '16px'
+}
+
+export const margin_bottom_20 = {
+    marginBottom: '20px'
+}
+
+export const margin_bottom_40 = {
+    marginBottom: '40px'
+}
+
+export const margin_top_60 = {
+    marginTop: '60px'
+}
+
+export const margin_top_14 = {
+    marginTop: '14px'
+}
