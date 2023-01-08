3cb91786cf5e2a3725aa30a16def4550633bb741
hansolbangul <ruaenddl981028@naver.com>
2023-01-09 00:37:51 +0900

    FEAT: Font css add

---
+++ src/css/font.js
@@ -0,0 +1,19 @@
+export const font3 = {
+    fontSize: '3vmin'
+}
+
+export const font5 = {
+    fontSize: '5vmin'
+}
+
+export const font4 = {
+    fontSize: '4vmin'
+}
+
+export const font8 = {
+    fontSize: '8vmin'
+}
+
+export const font10 = {
+    fontSize: '10vmin'
+}
